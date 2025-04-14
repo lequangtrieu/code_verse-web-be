48d152b3c4a6fc632bf1ff1ed170f0b144c89dd2
lequangtrieu <trieulqde160447@gmail.com>
2025-04-14 20:45:02 +0700

    update source

--- src/main/java/codeverse/com/web_be/config/SystemConfig/EmailService.java
+++ src/main/java/codeverse/com/web_be/config/SystemConfig/EmailService.java
@@ -1,14 +1,9 @@
 package codeverse.com.web_be.config.SystemConfig;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
 
 import java.util.Properties;
 

04ca6b5a24a9375688c68f34396ef7ba7075a13b
Levando10 <dolevan055@gmail.com>
2025-04-23 19:05:16 +0700

    update add to cart

---
+++ src/main/java/codeverse/com/web_be/dto/response/CourseResponse/CourseResponse.java
@@ -0,0 +1,37 @@
+package codeverse.com.web_be.dto.response.CourseResponse;
+
+import codeverse.com.web_be.entity.Course;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class CourseResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String thumbnailUrl;
+    private String level;
+    private String category;
+    private BigDecimal price;
+    private float rating;
+    private int totalStudents;
+    private boolean isTrending;
+
+    public static CourseResponse fromEntity(Course course) {
+        return CourseResponse.builder()
+                .id(course.getId())
+                .title(course.getTitle())
+                .description(course.getDescription())
+                .thumbnailUrl(course.getThumbnailUrl())
+                .level(course.getLevel().name())
+                .category(course.getCategory().getName())
+                .price(course.getPrice())
+                .rating(course.getRating())
+                .totalStudents(course.getTotalStudents())
+                .isTrending(course.isTrending())
+                .build();
+    }
+}
--- src/main/java/codeverse/com/web_be/repository/CourseRepository.java
+++ src/main/java/codeverse/com/web_be/repository/CourseRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findByInstructorId(Long instructorId);
+    List<Course> findAllByIsDeletedFalseAndIsPublishedTrue();
 }

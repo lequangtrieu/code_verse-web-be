866e90ad1c749a021aad6056815d6fa9a0bf883f
lequangtrieu <trieulqde160447@gmail.com>
2025-05-09 00:39:36 +0700

    update database, dummy data, view list course

--- src/main/java/codeverse/com/web_be/repository/ExerciseTaskRepository.java
+++ src/main/java/codeverse/com/web_be/repository/ExerciseTaskRepository.java
@@ -2,6 +2,8 @@ package codeverse.com.web_be.repository;
 
 import codeverse.com.web_be.entity.ExerciseTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExerciseTaskRepository extends JpaRepository<ExerciseTask, Long> {
 }

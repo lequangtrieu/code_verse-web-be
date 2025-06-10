9e7abbdffc14e8bc1bf3c6d2584f0194905cc7a4
lequangtrieu <trieulqde160447@gmail.com>
2025-06-10 23:16:02 +0700

    update CourseRepository.java

--- src/main/java/codeverse/com/web_be/repository/CourseRepository.java
+++ src/main/java/codeverse/com/web_be/repository/CourseRepository.java
@@ -27,17 +27,23 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
             "FROM Course c " +
             "LEFT JOIN c.category cat " +
             "LEFT JOIN c.instructor u " +
-            "WHERE (:id IS NULL OR c.id = :id) AND c.isDeleted = false AND c.status = \"PUBLISHED\"")
-    List<CourseResponse> selectCourses(@Param("id") Long id);
+            "WHERE c.isDeleted = false AND c.status = \"PUBLISHED\"")
+    List<CourseResponse> selectAllCourses();
 
-    default List<CourseResponse> selectAllCourses() {
-        return selectCourses(null);
-    }
-
-    default CourseResponse selectCourseById(Long id) {
-        List<CourseResponse> results = selectCourses(id);
-        return results.isEmpty() ? null : results.get(0);
-    }
+    @Query("SELECT new codeverse.com.web_be.dto.response.CourseResponse.CourseResponse(" +
+            "c.id, c.title, c.description, c.thumbnailUrl, CAST(c.level AS string), cat.name as category, c.price, c.discount, " +
+            "u.name, " +
+            "(SELECT COUNT(l) FROM Lesson l JOIN l.courseModule ms WHERE ms.course = c) as totalLessons, " +
+            "(SELECT COALESCE(ROUND(AVG(cr.rating), 1), 0) FROM CourseRating cr WHERE cr.course = c) as rating, " +
+            "(SELECT COUNT(cr) FROM CourseRating cr WHERE cr.course = c) as ratingCount, " +
+            "(SELECT COUNT(DISTINCT pt.user) FROM CourseEnrollment pt WHERE pt.course = c) as totalStudents, " +
+            "false as isTrending, " +
+            "(SELECT COALESCE(SUM(l.duration), 0) FROM Lesson l JOIN l.courseModule ms WHERE ms.course = c) as totalDurations) " +
+            "FROM Course c " +
+            "LEFT JOIN c.category cat " +
+            "LEFT JOIN c.instructor u " +
+            "WHERE c.id = :id AND c.isDeleted = false AND c.status = \"PUBLISHED\"")
+    CourseResponse selectCourseById(Long id);
 
     @Query("SELECT new codeverse.com.web_be.dto.response.CourseResponse.CourseProgressResponse(" +
             "c.id, c.title, c.description, c.thumbnailUrl, CAST(c.level AS string), cat.name, u.name, " +

48691c8dd9ec694475c2d78327addcaaa13aa0ee
tutien2904 <tutien29042003@gmail.com>
2025-06-12 23:53:03 +0700

    update management

--- src/main/java/codeverse/com/web_be/repository/UserRepository.java
+++ src/main/java/codeverse/com/web_be/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package codeverse.com.web_be.repository;
 
 import codeverse.com.web_be.entity.User;
+import codeverse.com.web_be.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     Optional<User> findByVerificationToken(String verificationToken);
+    @Query("SELECT u FROM User u WHERE u.role = 'INSTRUCTOR' AND u.isActiveInstructor = false")
+    List<User> findInactiveInstructors();
 }
 
--- src/main/java/codeverse/com/web_be/service/UserService/UserServiceImpl.java
+++ src/main/java/codeverse/com/web_be/service/UserService/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package codeverse.com.web_be.service.UserService;
 
+import codeverse.com.web_be.dto.request.CourseRequest.CourseUpdateRequest;
 import codeverse.com.web_be.dto.request.UserRequest.UserCreationByAdminRequest;
 import codeverse.com.web_be.dto.request.UserRequest.UserUpdateRequest;
+import codeverse.com.web_be.dto.response.SystemResponse.ApiResponse;
 import codeverse.com.web_be.dto.response.UserResponse.UserDetailResponse;
 import codeverse.com.web_be.dto.response.UserResponse.UserResponse;
 import codeverse.com.web_be.entity.User;
@@ -12,11 +14,15 @@ import codeverse.com.web_be.mapper.UserMapper;
 import codeverse.com.web_be.repository.UserRepository;
 import codeverse.com.web_be.service.FirebaseService.FirebaseStorageService;
 import codeverse.com.web_be.service.GenericServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -138,4 +144,22 @@ public class UserServiceImpl extends GenericServiceImpl<User, Long> implements I
         return userMapper.userToUserDetailResponse(user);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<UserDetailResponse> getInactiveInstructors() {
+        List<User> inactiveInstructors = userRepository.findInactiveInstructors();
+
+        return inactiveInstructors.stream()
+                .map(userMapper::userToUserDetailResponse)
+                .toList();
+    }
+
+    @Override
+    public void activateInstructor(Long instructorId) {
+        User user = userRepository.findById(instructorId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        // Update isActiveInstructor to true (1)
+        user.setIsActiveInstructor(true);
+        userRepository.save(user);
+    }
 }

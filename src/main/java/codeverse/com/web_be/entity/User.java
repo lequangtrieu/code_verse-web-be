3dc300c2a3fd10429802fc1a44f06036e9c1124a
Levando10 <dolevan055@gmail.com>
2025-06-14 10:43:18 +0700

    update conflic

--- src/main/java/codeverse/com/web_be/entity/User.java
+++ src/main/java/codeverse/com/web_be/entity/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
--- src/main/java/codeverse/com/web_be/service/CourseService/CourseServiceImpl.java
+++ src/main/java/codeverse/com/web_be/service/CourseService/CourseServiceImpl.java
@@ -6,12 +6,6 @@ import codeverse.com.web_be.dto.request.CourseRequest.CourseUpdateRequest;
 import codeverse.com.web_be.dto.response.CourseModuleResponse.CourseModuleValidationResponse;
 import codeverse.com.web_be.dto.response.CourseResponse.CourseForUpdateResponse;
 import codeverse.com.web_be.dto.response.CourseResponse.CourseProgressResponse;
-import codeverse.com.web_be.dto.request.ExerciseRequest.ExerciseTaskFullCreateRequest;
-import codeverse.com.web_be.dto.request.ExerciseRequest.ExerciseTaskUpdateRequest;
-import codeverse.com.web_be.dto.request.LessonRequest.LessonFullCreateRequest;
-import codeverse.com.web_be.dto.request.LessonRequest.LessonUpdateRequest;
-import codeverse.com.web_be.dto.request.CourseModuleRequest.CourseModuleFullCreateRequest;
-import codeverse.com.web_be.dto.request.CourseModuleRequest.CourseModuleUpdateRequest;
 import codeverse.com.web_be.dto.response.CourseResponse.*;
 import codeverse.com.web_be.entity.*;
 import codeverse.com.web_be.enums.LessonProgressStatus;
@@ -63,13 +57,7 @@ public class CourseServiceImpl extends GenericServiceImpl<Course, Long> implemen
                              CourseEnrollmentRepository courseEnrollmentRepository,
                              TestCaseRepository testCaseRepository,
                              QuizQuestionRepository quizQuestionRepository,
-                             QuizAnswerRepository quizAnswerRepository
-                             CourseModuleMapper courseModuleMapper,
-                             LessonMapper lessonMapper,
-                             ExerciseMapper exerciseMapper,
-                             ExerciseTaskMapper exerciseTaskMapper,
-                             TheoryMapper theoryMapper,
-                             CourseEnrollmentRepository courseEnrollmentRepository,
+                             QuizAnswerRepository quizAnswerRepository,
                              LessonProgressRepository lessonProgressRepository, UserRepository userRepository
     ) {
         super(courseRepository);
@@ -263,117 +251,6 @@ public class CourseServiceImpl extends GenericServiceImpl<Course, Long> implemen
 
         course.setStatus(request.getStatus());
         courseRepository.save(course);
-    }
-
-
-    @Override
-    public CourseDetailDTO getCourseDetails(Long courseId, Long userId) {
-        Course course = courseRepository.findById(courseId)
-                .orElseThrow(() -> new RuntimeException("Course not found with id: " + courseId));
-        List<CourseModuleDTO> modules = courseRepository.getModulesByCourseId(courseId);
-        List<LessonProgress> progresses = courseRepository.findByUserIdAndCourseId(userId, courseId);
-
-        Map<Long, LessonProgress> progressMap = new HashMap<>();
-        for (LessonProgress progress : progresses) {
-            progressMap.put(progress.getLesson().getId(), progress);
-        }
-
-        for (CourseModuleDTO module : modules) {
-            List<LessonDTO> lessons = courseRepository.getLessonsByModuleId(module.getId());
-
-            for (LessonDTO lesson : lessons) {
-                LessonProgress progress = progressMap.get(lesson.getId());
-                if (progress != null) {
-                    lesson.setStatus(progress.getStatus());
-                    if (progress.getCodeSubmission() != null) {
-                        lesson.setCode(progress.getCodeSubmission().getCode());
-                    }
-                } else {
-                    lesson.setStatus(LessonProgressStatus.NOT_STARTED);
-                }
-
-                if (Objects.equals(lesson.getLessonType(), LessonType.EXAM)) {
-                    List<QuestionDTO> questionDTO = courseRepository.getQuestionByLessonId(lesson.getId());
-                    for (QuestionDTO question : questionDTO) {
-                        question.setAnswers(courseRepository.getAnswersByQuestionID(question.getId()));
-                    }
-
-                    lesson.setQuestions(questionDTO);
-                } else {
-                    TheoryDTO theory = courseRepository.getTheoryByLessonId(lesson.getId());
-                    ExerciseDTO exerciseDTO = courseRepository.getExerciseByLessonId(lesson.getId());
-
-                    if (exerciseDTO != null) {
-                        List<TaskDTO> taskDTOS = courseRepository.getExerciseTaskByExerciseID(exerciseDTO.getId());
-                        List<TestCaseDTO> testCaseDTO = courseRepository.getTestCaseByExerciseId(exerciseDTO.getId());
-                        List<String> tasks = new ArrayList<>();
-                        for (TaskDTO taskDTO : taskDTOS) {
-                            tasks.add(taskDTO.getDescription());
-                        }
-                        exerciseDTO.setTasks(tasks);
-                        lesson.setTestCases(testCaseDTO);
-                        lesson.setExercise(exerciseDTO);
-                    } else {
-                        lesson.setExercise(null);
-                    }
-
-                    lesson.setTheory(theory);
-                }
-
-
-            }
-            module.setSubLessons(lessons);
-        }
-        CourseDetailDTO courseDetailDTO = new CourseDetailDTO();
-        courseDetailDTO.setData(modules);
-        courseDetailDTO.setLanguage(course.getLanguage());
-        return courseDetailDTO;
-    }
-
-    @Override
-    public String submitCodeHandler(CodeRequestDTO request) {
-        Long userId = request.getUserId();
-        Long lessonId = request.getLessonId();
-
-        LessonProgress lessonProgress = lessonProgressRepository
-                .findByUserIdAndLessonId(userId, lessonId)
-                .orElseGet(() -> {
-                    Lesson lesson = lessonRepository.findById(lessonId)
-                            .orElseThrow(() -> new RuntimeException("Lesson not found"));
-                    User user = userRepository.findById(userId)
-                            .orElseThrow(() -> new RuntimeException("User not found"));
-
-                    return lessonProgressRepository.save(
-                            LessonProgress.builder()
-                                    .lesson(lesson)
-                                    .user(user)
-                                    .status(LessonProgressStatus.PASSED)
-                                    .startedAt(LocalDateTime.now())
-                                    .build()
-                    );
-                });
-
-        CodeSubmission existingSubmission = lessonProgress.getCodeSubmission();
-
-        if (existingSubmission != null) {
-            existingSubmission.setCode(request.getCode());
-            existingSubmission.setExecutionTime(request.getExecutionTime());
-            existingSubmission.setMemoryUsage(request.getMemoryUsage());
-        } else {
-            CodeSubmission newSubmission = CodeSubmission.builder()
-                    .lessonProgress(lessonProgress)
-                    .code(request.getCode())
-                    .executionTime(request.getExecutionTime())
-                    .memoryUsage(request.getMemoryUsage())
-                    .build();
-
-            lessonProgress.setCodeSubmission(newSubmission);
-        }
-
-        lessonProgress.setStatus(LessonProgressStatus.PASSED);
-        lessonProgressRepository.save(lessonProgress);
-
-        return "submitted";
     }
 
 
